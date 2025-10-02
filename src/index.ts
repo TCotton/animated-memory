1491c9853c8a27b786c5dca77b4d08bfb4a15795
TCotton <me@andywalpole.me>
2025-10-02 10:14:06 +0100

    initial commit

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,54 @@
 import "./index.css";
 
 document.querySelector("#root")!.innerHTML = `
-<div class="content">
-  <h1>Vanilla Rspack</h1>
-  <p>Start building amazing things with Rspack.</p>
+<div is-="typography-block">
+  <h1>ANDREW WALPOLE</h1>
+  <h2>TECH LEAD | FULL-STACK ENGINEER | AWS CERTIFIED EXPERT</h2>
+  
+  <section is-="typography-block">
+    <h1>Get future-ready</h1>
+    <p>As a technology leader and engineer, I view programming as a product-centric activity. My expertise encompasses both back-end and front-end development, emphasising serverless, event-driven architecture and systems thinking. On the front-end, I strive for solutions that combine accessibility, usability, and performance. I enthusiastically endorse The Agile Manifesto and the systems thinking and domain-driven approach now prevalent in our industry. Over my career, I’ve played instrumental roles in significant retail tech initiatives including projects for Tesco, Boots, and Sainsbury's.</p
+
+    <p>Most recently, I guided tech teams for NBrown’s SimplyBe and Castle projects.</p> 
+
+    <ul marker-="pen tree open">
+        <li>Positioned the fintech project, Castle, to launch with its first customers in January 2026.</li>
+        <li>Delivered full platform relaunch of Simply Be in the autumn of 2022; guided three cross-functional squads in creating a React.js/Next.js monorepo from several different repos, resulting in a successful go-live and improvements in platform performance and stability.</li>
+        <li>The successful work on the “new web” and the Castle project directly lead to a £181m takeover of NBrown in December 2024.</li>
+    </ul>
+
+    <p>I was also pivotal in creating Elevaate, which secured £600k in funding and won Startup of the Year at Retail Week Live 2015.</p>
+
+    </p>My experience spans all levels, from infrastructure to the browser</p>
+  </section>
+  
+<section>
+<h1>Services Offered</h1>
+
+<section>
+Full-Stack Development
+I craft comprehensive web solutions with a focus on user-centric design.
+<article>
+
+<article>
+v
+</article></articlce>
+
+</article></articlce>
+</section>
+
+<section>
+
+</section>
+  
+</section>
+   
+   <article is-="typography-block">
+    <h1>Experience</h1>
+    </article>
+  
 </div>
 `;
+
+
+
